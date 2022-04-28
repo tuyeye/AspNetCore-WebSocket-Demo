181ec94fce0d12301752f058993e75d982c1a04d
yeyetu <sanqii@icloud.com>
2022-04-28 17:36:16 +0800

    新建工程

---
+++ Helps/TheWebSocket/WebSocketModels.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace openapi.Helps.TheWebSocket
+{
+
+    public class WebsocketClient
+    {
+        public WebSocket WebSocket { get; set; }
+
+        public string Id { get; set; }
+
+        public string RoomNo { get; set; }
+
+        public Task SendMessageAsync(string message)
+        {
+            var msg = Encoding.UTF8.GetBytes(message);
+            return WebSocket.SendAsync(new ArraySegment<byte>(msg, 0, msg.Length), WebSocketMessageType.Text, true,CancellationToken.None);
+        }
+    }
+
+    public class Message
+    {
+        public string SendClientId { get; set; }
+
+        public string action { get; set; }
+
+        public string msg { get; set; }
+
+        public string nick { get; set; }
+    }
+
+
+}
---
+++ Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using openapi.Helps.TheWebSocket;
+
+namespace openapi
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        [Obsolete]
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseWebSockets(new WebSocketOptions
+            {
+                KeepAliveInterval = TimeSpan.FromSeconds(60),
+                ReceiveBufferSize = 1 * 1024
+            });
+            app.UseMiddleware<WebsocketHandlerMiddleware>();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+
+    }
+}
